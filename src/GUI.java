8d831821ac2c6a1aacf4a7642f5c6cffa4268809
Avikash-Mishra <avimish62@gmail.com>
2013-08-23 20:37:43 +1000

    maximise screen

--- src/GUI.java
+++ src/GUI.java
@@ -2,5 +2,15 @@ import javax.swing.JFrame;
 
 
 public class GUI extends JFrame {
+	
+	public GUI(){
+		this.setExtendedState(this.getExtendedState() | this.MAXIMIZED_BOTH);
+		this.setVisible(true);
+		
+	}
+	
+	public static void main(String[] args){
+		GUI gui = new GUI();
+	}
 
 }
