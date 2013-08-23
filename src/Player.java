052e95b7226c1e94ab62c550e2b89b22ba63c335
craigthelinguist <sinroth@gmail.com>
2013-08-23 22:00:37 +1200

    updated player class

--- src/Player.java
+++ src/Player.java
@@ -1,21 +1,80 @@
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
 
 
 public class Player {
 	
-	double posX;
-	double posY;
+	private int posX;
+	private int posY;
 	
-	int health;
-	int sobriety;
+	private int WIDTH;
+	private int HEIGHT;
+	private Rectangle boundingBox;
 	
-	public Player(){
+	private int health;
+	private int sobriety;
+	
+	private BufferedImage model;
+	private ImageObserver modelObserver;
+	
+	public Player(BufferedImage model){
+		this.model = model;
+		this.WIDTH = model.getWidth();
+		this.HEIGHT = model.getHeight();
+		this.boundingBox = new Rectangle(WIDTH,HEIGHT);
 	}
 	
 	public void draw(Graphics g){
-		
+		g.drawImage(model,posX,posY,null);
 	}
 	
+	public boolean collision(Rectangle other){
+		return boundingBox.intersects(other);
+	}
 	
+	/** MOVEMENT */
+	public void moveUp(){
+		posY-=2;
+	}
+	
+	public void moveDown(){
+		posY+=2;
+	}
+	
+	public void moveLeft(){
+		posX-=2;
+	}
+	
+	public void mouseRight(){
+		posX +=2;
+	}
+
+	/** GETTERS AND SETTERS */
+	public double getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public double getPosY() {
+		return posY;
+	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public void setSobriety(int sobriety) {
+		this.sobriety = sobriety;
+	}
 	
 }
