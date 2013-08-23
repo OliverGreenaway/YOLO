d458aeefc73843afa31a1da6393de5cd29b50863
Avikash-Mishra <avimish62@gmail.com>
2013-08-23 20:41:58 +1000

    added player class

--- src/GUI.java
+++ src/GUI.java
@@ -6,8 +6,9 @@ public class GUI extends JFrame {
 	public GUI(){
 		this.setExtendedState(this.getExtendedState() | this.MAXIMIZED_BOTH);
 		this.setVisible(true);
-		
 	}
+	
+	
 	
 	public static void main(String[] args){
 		GUI gui = new GUI();
