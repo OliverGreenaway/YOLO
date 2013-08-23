166f6583996da080b057632701f9cb4d88582802
Avikash-Mishra <avimish62@gmail.com>
2013-08-23 21:20:26 +1000

    added canvas to GUI

--- src/GUI.java
+++ src/GUI.java
@@ -2,10 +2,13 @@ import javax.swing.JFrame;
 
 
 public class GUI extends JFrame {
-	
+	Canvas canvas;
 	public GUI(){
 		this.setExtendedState(this.getExtendedState() | this.MAXIMIZED_BOTH);
 		this.setVisible(true);
+		this.canvas = new Canvas(this);
+		this.add(canvas);
+		
 	}
 	
 	
