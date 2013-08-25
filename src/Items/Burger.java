6ac41cdf98716d20f8ad08003ed72dad7dfc96f4
craigthelinguist <sinroth@gmail.com>
2013-08-25 13:05:39 +1200

    items generating

---
+++ src/Items/Burger.java
@@ -0,0 +1,20 @@
+package Items;
+
+import java.awt.image.BufferedImage;
+
+import Core.Player;
+
+public class Burger extends Item implements PickUpItem {
+
+	public Burger(BufferedImage image) {
+		super(image);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void playerConsume(Player p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/Items/Shrooms.java
@@ -0,0 +1,20 @@
+package Items;
+
+import java.awt.image.BufferedImage;
+
+import Core.Player;
+
+public class Shrooms extends Item implements PickUpItem {
+
+	public Shrooms(BufferedImage image) {
+		super(image);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void playerConsume(Player p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
